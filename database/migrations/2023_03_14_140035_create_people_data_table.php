9582a5d2431ddb36c412e28b675559035572cfdf
Julian Rodriguez <julianrodriguez19961@gmail.com>
2023-03-14 16:41:12 -0500

    feat: :tada: Inicio de modificación al modulo de usuarios
    Se realizará un middleware , que funcione para los diferentes procesos en los cuales se reciba el objeto formulario con los datos necesarios para la tabla peole_data(ref terceros_medisof) y crear primero este registro antes de un registro de usuario con el fin de recuperar la clave del mismo.

---
+++ database/migrations/2023_03_14_140035_create_people_data_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('people_data', function (Blueprint $table) {
+            
+            $table->id();
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('document')->unique();
+            $table->string('document_city')->nullable();
+            $table->string('document_type_id');
+            $table->string('regime_id')->nullable();
+            $table->string('city')->nullable();
+            $table->string('legal_representation_id')->nullable();
+            $table->string('organitation_path')->nullable();
+            $table->string('comments')->nullable();
+            $table->date('born_date')->nullable();
+            $table->tinyInteger('age')->default(0);
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+
+        Schema::dropIfExists('people_data');
+        
+    }
+};
